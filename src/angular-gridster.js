3a7d162d83ab3684d032b0161117dd2eaba20b55
David Barreto <barretollano@gmail.com>
2014-12-09 17:32:03 -0430

    Added event 'gridster-item-initialized'

--- src/angular-gridster.js
+++ src/angular-gridster.js
@@ -800,6 +800,20 @@ var refresh = function() {
 				this.$element.css('width', (this.sizeX * this.gridster.curColWidth - this.gridster.margins[1]) + 'px');
 			}
 		};
+
+		/**
++		 * Gets an element's width
++		 */
++		this.getElementSizeX = function(){
++			return (this.sizeX * this.gridster.curColWidth - this.gridster.margins[1]);
++		};
++
++		/**
++		 * Gets an element's height
++		 */
++		this.getElementSizeY = function(){
++			return (this.sizeY * this.gridster.curRowHeight - this.gridster.margins[0]);
++		};
 	})
 
 	.factory('GridsterDraggable', ['$document', '$timeout', '$window',
@@ -1378,6 +1392,8 @@ var refresh = function() {
 					for (var i = 0, l = aspects.length; i < l; ++i) {
 						aspectFn(aspects[i]);
 					}
+
+					scope.$broadcast('gridster-item-initialized', [item.sizeY, item.sizeX, item.getElementSizeY(), item.getElementSizeX()]);
 
 					function positionChanged() {
 						// call setPosition so the element and gridster controller are updated
